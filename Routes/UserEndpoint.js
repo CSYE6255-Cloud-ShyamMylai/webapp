2a38b85551e168b5bd1e6535dfc8f10fac11d04f
Shyam Sundar Mylai <mylai.s@northeastern.edu>
2024-03-19 15:02:12 -0400

    used warns and changed to function method

--- Routes/UserEndpoint.js
+++ Routes/UserEndpoint.js
@@ -11,15 +11,16 @@ app.use(express.json())
 app.get('/self', [(req, res, next) => {
     if (Object.keys(req.query).length != 0 || req._body == true || req.get('Content-length') != undefined ||
     req.get('accept') != 'application/json' ){
-        logger.log({
-            level: 'error',
-            message: `Failed due to invalid request body or headers`,
-            metadata: {
-                method: req.method,
-                path: req.baseUrl + req.path,
-                status: 400
-            }
-        })
+        // logger.log({
+        //     level: 'error',
+        //     message: `Failed due to invalid request body or headers`,
+        //     metadata: {
+        //         method: req.method,
+        //         path: req.baseUrl + req.path,
+        //         status: 400
+        //     }
+        // })
+        logger.error("Failed due to invalid request body or headers", {method: req.method, path: req.baseUrl + req.path, status: 400});
         return res.status(400).send();
     }
     next();
@@ -33,28 +34,30 @@ app.get('/self', [(req, res, next) => {
         })
         // res.setHeader('Content-Type', 'application/json');
         res.setHeader('Accept', 'application/json');
-        logger.log({
-            level: 'info',
-            message: `User with username ${req.username} has been fetched`,
-            metadata: {
-                method: req.method,
-                path: req.baseUrl + req.path,
-                status: 200
-            }
-        })
+        // logger.log({
+        //     level: 'info',
+        //     message: `User with username ${req.username} has been fetched`,
+        //     metadata: {
+        //         method: req.method,
+        //         path: req.baseUrl + req.path,
+        //         status: 200
+        //     }
+        // })
+        logger.info(`User with username ${req.username} has been fetched`, {method: req.method, path: req.baseUrl + req.path, status: 200});
         return res.status(200).send(response);
     }
     catch (err) {
-        logger.log({
-            level: 'error',
-            message: `User with username ${req.username} couldn't be fetched (UserEndpoint.js)`,
-            metadata: {
-                method: req.method,
-                path: req.baseUrl + req.path,
-                status: 400,
-                error: err
-            }
-        })
+        // logger.log({
+        //     level: 'error',
+        //     message: `User with username ${req.username} couldn't be fetched (UserEndpoint.js)`,
+        //     metadata: {
+        //         method: req.method,
+        //         path: req.baseUrl + req.path,
+        //         status: 400,
+        //         error: err
+        //     }
+        // })
+        logger.error(`User with username ${req.username} couldn't be fetched (UserEndpoint.js)`, {method: req.method, path: req.baseUrl + req.path, status: 400, error: err});  
         return res.send(400).send()
     }
 })
@@ -63,15 +66,16 @@ app.put('/self', [(req, res, next) => {
     //inline check for the body before dbCheck and authCheck 
     const { password, first_name, last_name, username, ...anythingelse } = req.body;
     if (username != undefined || (!password && !first_name && !last_name) || Object.keys(req.query).length != 0 || Object.keys(anythingelse).length != 0  || req.get('content-type') != 'application/json'){
-        logger.log({
-            level: 'error',
-            message: `Failed due to invalid request body or headers check the body for the put request`,
-            metadata: {
-                method: req.method,
-                path: req.baseUrl + req.path,
-                status: 400
-            }
-        })
+        // logger.log({
+        //     level: 'warn',
+        //     message: `Failed due to invalid request body or headers check the body for the put request`,
+        //     metadata: {
+        //         method: req.method,
+        //         path: req.baseUrl + req.path,
+        //         status: 400
+        //     }
+        // })
+        logger.warn("Failed due to invalid request body or headers check the body for the put request", {method: req.method, path: req.baseUrl + req.path, status: 400});
         return res.status(400).send();
     }
     next();
@@ -98,30 +102,32 @@ app.put('/self', [(req, res, next) => {
                 // password: password ? password : userForUpdation.password
             });
         }
-        logger.log({
-            level: 'info',
-            message: `User with username ${req.username} has been updated`,
-            metadata: {
-                method: req.method,
-                path: req.baseUrl + req.path,
-                status: 204
-            }
-        })
+        // logger.log({
+        //     level: 'info',
+        //     message: `User with username ${req.username} has been updated`,
+        //     metadata: {
+        //         method: req.method,
+        //         path: req.baseUrl + req.path,
+        //         status: 204
+        //     }
+        // })
+        logger.info(`User with username ${req.username} has been updated`, {method: req.method, path: req.baseUrl + req.path, status: 204});
         return res.status(204).send();
 
     }
     catch (err) {
         console.log(err);
-        logger.log({
-            level: 'error',
-            message: `User with username ${req.username} couldn't be updated (UserEndpoint.js)`,
-            metadata: {
-                method: req.method,
-                path: req.baseUrl + req.path,
-                status: 400,
-                error: err
-            }
-        })
+        // logger.log({
+        //     level: 'error',
+        //     message: `User with username ${req.username} couldn't be updated (UserEndpoint.js)`,
+        //     metadata: {
+        //         method: req.method,
+        //         path: req.baseUrl + req.path,
+        //         status: 400,
+        //         error: err
+        //     }
+        // })
+        logger.error(`User with username ${req.username} couldn't be updated (UserEndpoint.js)`, {method: req.method, path: req.baseUrl + req.path, status: 400, error: err});
         return res.status(400).send();
     }
 
@@ -129,41 +135,43 @@ app.put('/self', [(req, res, next) => {
 
 app.post('/', [
     (req, res, next) => {
-        logger.log({
-            level: 'debug',
-            message: `entered middleware for checking the request body and headers`,
-            metadata: {
-                method: req.method,
-                path: req.baseUrl + req.path,
-                // status: 201
-            }
-        
-        })
+        // logger.log({
+        //     level: 'debug',
+        //     message: `entered middleware for checking the request body and headers`,
+        //     metadata: {
+        //         method: req.method,
+        //         path: req.baseUrl + req.path,
+        //         // status: 201
+        //     }
+        // })
+        logger.debug("entered middleware for checking the request body and headers", {method: req.method, path: req.baseUrl + req.path});
         if (req._body == false || req.get('Content-length') == undefined || Object.keys(req.query).length != 0
         || req.get('accept') != 'application/json' || req.get('content-type') != 'application/json') {
-            logger.log({
-                level: 'error',
-                message: `Failed due to invalid request body or headers`,
-                metadata: {
-                    method: req.method,
-                    path: req.baseUrl + req.path,
-                    status: 400
-                }
-            })
+            // logger.log({
+            //     level: 'error',
+            //     message: `Failed due to invalid request body or headers`,
+            //     metadata: {
+            //         method: req.method,
+            //         path: req.baseUrl + req.path,
+            //         status: 400
+            //     }
+            // })
+            logger.warn("Failed due to invalid request body or headers", {method: req.method, path: req.baseUrl + req.path, status: 400});
             return res.status(400).send();
         }
         if(req.headers.authorization != undefined){
             const [username, password] = Buffer.from(req.headers.authorization.split(' ')[1], 'base64').toString().split(":");
             if((username && username.length>0) ||( password &&  password.length>0)){
-                logger.log({
-                    level: 'error',
-                    message: `Authorization header is not required`,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }   
-                })
+                // logger.log({
+                //     level: 'error',
+                //     message: `Authorization header is not required`,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }   
+                // })
+                logger.warn("Authorization header is not required", {method: req.method, path: req.baseUrl + req.path, status: 400});
                 return res.status(400).send({message:"Authorization header is not required"});
             }
         }
@@ -172,75 +180,81 @@ app.post('/', [
         const { first_name, last_name, username, password, ...anythingelse } = req.body;
         switch (true) {
             case !first_name && !last_name && !username && !password:
-                logger.log({
-                    level: 'error',
-                    message: `firstname, lastname , username and password are missing in the body`,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }
-                })
+                // logger.log({
+                //     level: 'error',
+                //     message: `firstname, lastname , username and password are missing in the body`,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }
+                // })
+                logger.warn("firstname, lastname , username and password are missing in the body", {method: req.method, path: req.baseUrl + req.path, status: 400});   
                 res.status(400).send({ message: "All fields required are missing in the body" });
                 break;
             case !first_name:
-                logger.log({
-                    level: 'error',
-                    message: `First name is missing in the body`,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }
-                })
+                // logger.log({
+                //     level: 'error',
+                //     message: `First name is missing in the body`,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }
+                // })
+                logger.warn("First name is missing in the body", {method: req.method, path: req.baseUrl + req.path, status: 400});
                 res.status(400).send({ message: "First name is missing in the body" });
                 break;
             case !last_name:
-                logger.log({
-                    level: 'error',
-                    message: `Last name is missing in the body`,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }
-                })
+                // logger.log({
+                //     level: 'error',
+                //     message: `Last name is missing in the body`,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }
+                // })
+                logger.warn("Last name is missing in the body", {method: req.method, path: req.baseUrl + req.path, status: 400});
                 res.status(400).send({ message: "Last name is missing in the body" });
                 break;
             case !username:
-                logger.log({
-                    level: 'error',
-                    message: `Username is missing in the body`,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }
-                })
+                // logger.log({
+                //     level: 'warn',
+                //     message: `Username is missing in the body`,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }
+                // })
+                logger.warn("Username is missing in the body", {method: req.method, path: req.baseUrl + req.path, status: 400});
                 res.status(400).send({ message: "Username is missing in the body" });
                 break;
             case !password:
-                logger.log({
-                    level: 'error',
-                    message: `Password is missing in the body`,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }
-                })
+                // logger.log({
+                //     level: 'error',
+                //     message: `Password is missing in the body`,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }
+                // })
+                logger.warn("Password is missing in the body", {method: req.method, path: req.baseUrl + req.path, status: 400});
                 res.status(400).send({ message: "Password is missing in the body" });
                 break;
             case Object.keys(anythingelse).length != 0:
-                logger.log({
-                    level: 'error',
-                    message: `Other properties are present but shouldn't be there`,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }
-                })
+                // logger.log({
+                //     level: 'error',
+                //     message: `Other properties are present but shouldn't be there`,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }
+                // })
+                logger.warn("Other properties are present but shouldn't be there", {method: req.method, path: req.baseUrl + req.path, status: 400});
                 res.status(400).send({ message: "Other properties shouldn't be present" })
             default:
                 next();
@@ -250,15 +264,16 @@ app.post('/', [
             const { first_name, last_name, username, password, ...anythingelse } = req.body;
             const emailCheck = await User.count({ where: { username: username } });
             if (emailCheck > 0) {
-                logger.log({
-                    level: 'error',
-                    message: `Username ${username} already exists hence failed to create user `,
-                    metadata: {
-                        method: req.method,
-                        path: req.baseUrl + req.path,
-                        status: 400
-                    }
-                })
+                // logger.log({
+                //     level: 'error',
+                //     message: `Username ${username} already exists hence failed to create user `,
+                //     metadata: {
+                //         method: req.method,
+                //         path: req.baseUrl + req.path,
+                //         status: 400
+                //     }
+                // })
+                logger.warn(`Username ${username} already exists hence failed to create user `, {method: req.method, path: req.baseUrl + req.path, status: 400});
                 return res.status(400).send({ message: "Username already exists" });
             }
             const creationResponse = await User.create({
@@ -267,15 +282,16 @@ app.post('/', [
                 username: username,
                 password: password,
             });
-            logger.log({
-                level: 'info',
-                message: `User with username ${username} has been created`,
-                metadata: {
-                    method: req.method,
-                    path: req.baseUrl + req.path,
-                    status: 201
-                }
-            })
+            // logger.log({
+            //     level: 'info',
+            //     message: `User with username ${username} has been created`,
+            //     metadata: {
+            //         method: req.method,
+            //         path: req.baseUrl + req.path,
+            //         status: 201
+            //     }
+            // })
+            logger.info(`User with username ${username} has been created`, {method: req.method, path: req.baseUrl + req.path, status: 201});
             // res.setHeader('Content-Type', 'application/json');
             res.setHeader('Accept', 'application/json');
             return res.status(201).send({
@@ -287,16 +303,17 @@ app.post('/', [
                 account_updated:creationResponse.account_updated,
             });
         } catch (err) {
-            logger.log({
-                level: 'error',
-                message: `User with username ${username} couldn't be created (UserEndpoint.js)`,
-                metadata: {
-                    method: req.method,
-                    path: req.baseUrl + req.path,
-                    status: 400,
-                    error: err
-                }
-            })
+            // logger.log({
+            //     level: 'error',
+            //     message: `User with username ${username} couldn't be created (UserEndpoint.js)`,
+            //     metadata: {
+            //         method: req.method,
+            //         path: req.baseUrl + req.path,
+            //         status: 400,
+            //         error: err
+            //     }
+            // })
+            logger.error(`User with username ${username} couldn't be created (UserEndpoint.js)`, {method: req.method, path: req.baseUrl + req.path, status: 400, error: err});
             return res.status(400).send({
                 message: err.message,
             });
